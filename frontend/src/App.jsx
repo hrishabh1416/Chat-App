79695ddf57f9f04f73843aa9e3a8379cdcad530e
hrishabh1416 <hrishabhshrivastava16@gmail.com>
2025-07-27 23:17:59 +0530

    Fix background image for production build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,47 @@
-  import React, { useContext } from 'react'
-  import Home from './components/pages/Homepage.jsx'
-  import Loginpage from './components/pages/Loginpage.jsx'
-  import Profilepage from './components/pages/Profilepage.jsx'
-  import { Route,Routes,BrowserRouter,useLocation,Navigate } from 'react-router-dom'
-  import { AuthContext, AuthProvider } from './context/AuthContext.jsx'
-  import {Toaster} from 'react-hot-toast';
+import React, { useContext } from 'react'
+import Home from './components/pages/Homepage.jsx'
+import Loginpage from './components/pages/Loginpage.jsx'
+import Profilepage from './components/pages/Profilepage.jsx'
+import { Route, Routes, BrowserRouter, useLocation, Navigate } from 'react-router-dom'
+import { AuthContext, AuthProvider } from './context/AuthContext.jsx'
+import { Toaster } from 'react-hot-toast'
 import { ChatProvider } from './context/ChatContext.jsx'
-  const  Allcontent = () => {
-    return (
-      <div className="bg-[url('./src/assets/background1.jpg')] bg-contain">
-        <Toaster/>
-        {/* <Home/> */}
-        {/* <Loginpage/> */}
-        <Profilepage/>  
-      </div>
-    )
-  }
-const MainApp=()=>{
-  const location=useLocation();
-  const {authUser}=useContext(AuthContext);
-  console.log("MainApp render - authUser:", authUser);
+import backgroundImage from './assets/background1.jpg' // ✅ import image
+
+const MainApp = () => {
+  const location = useLocation();
+  const { authUser } = useContext(AuthContext);
+
   return (
-    <div  className="bg-[url('./src/assets/background1.jpg')] bg-contain"> 
-      <Toaster/>
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <Toaster />
       <Routes>
-      <Route path='/' element={ authUser ? <Home/> : <Navigate to='/login'/>}/>
-      <Route path='/login' element={ !authUser ? <Loginpage/> : <Navigate to='/profile'/>} />
-      <Route path='/profile' element={ authUser ? <Profilepage/> : <Navigate to='/login'/>} />
+        <Route path='/' element={authUser ? <Home /> : <Navigate to='/login' />} />
+        <Route path='/login' element={!authUser ? <Loginpage /> : <Navigate to='/profile' />} />
+        <Route path='/profile' element={authUser ? <Profilepage /> : <Navigate to='/login' />} />
       </Routes>
     </div>
-    );
-}
-  function App(){
-    return (
+  );
+};
+
+function App() {
+  return (
     <BrowserRouter>
-    <AuthProvider>
-      <ChatProvider>
-    <MainApp/>
-    </ChatProvider>
-    </AuthProvider>
+      <AuthProvider>
+        <ChatProvider>
+          <MainApp />
+        </ChatProvider>
+      </AuthProvider>
     </BrowserRouter>
-    );
-  }
-  
-  export default App;
-  
+  );
+}
+
+export default App;
